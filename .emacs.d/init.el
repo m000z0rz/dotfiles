d77b8dbcb6d5029d6c7f13bf5829cbab12d659c5
Brian Baker <bbaker@epic.com>
2022-07-07 11:33:52 -0500

    Ensure lsp-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -275,6 +275,7 @@
 	      ("C-c p" . projectile-command-map)))
 
 (use-package lsp-mode
+  :ensure t
   :init
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   (setq lsp-keymap-prefix "C-c l")
