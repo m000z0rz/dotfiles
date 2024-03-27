23362d7c8e8bf4042badd3b07d9280e6d0eea453
Brian Baker <bbaker@epic.com>
2024-03-27 13:36:43 -0500

    formatting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,11 +30,11 @@
     (load-file custom-file)))
 
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-			backup-by-copying t ; Don't de-link hard links
-			version-control t ; Use version numbers on backups
-			delete-old-versions t ; Automatically delete excess backups
+			backup-by-copying t				 ; Don't de-link hard links
+			version-control t					 ; Use version numbers on backups
+			delete-old-versions t			 ; Automatically delete excess backups
 			kept-new-version 20
-			kept-old-version 5);
+			kept-old-version 5)
 
 (global-auto-revert-mode t)
 
